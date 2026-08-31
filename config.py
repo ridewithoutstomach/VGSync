55070ece99d33847752e88361255dd986bad8f05
ridewithoutstomach <bernd@casa-eller.de>
2026-08-31 19:45:26 +0200

    v6.01

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ from PySide6.QtCore import QSettings
 # 1) Versions-Konfiguration & Modus
 ##############################################################################
 
-APP_VERSION = "6.0"
+APP_VERSION = "6.01"
 # use 4.30_pre for a pre Version
 
 
